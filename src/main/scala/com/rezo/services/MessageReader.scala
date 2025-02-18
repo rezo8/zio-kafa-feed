f3e41bb0378d997f48ddfc0c26b9898204eb574f
Ribhi El-Zaru <ribhielzaru@gmail.com>
2025-02-18 11:47:41 +0300

    ensure auto commit is false just in case and remove commented out pre-zio code

--- src/main/scala/com/rezo/services/MessageReader.scala
+++ src/main/scala/com/rezo/services/MessageReader.scala
@@ -27,6 +27,7 @@ class MessageReader(config: KafkaConsumerConfig) {
     )
     props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
     props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
     props.put(
       ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
       "org.apache.kafka.common.serialization.StringDeserializer"
@@ -81,7 +82,6 @@ class MessageReader(config: KafkaConsumerConfig) {
             messagesProcessed = count
           }
 
-          // Process the records and decode them
           for (record <- records.asScala) {
             if (messagesProcessed < count) {
               decode[Person](record.value()) match {
@@ -102,53 +102,4 @@ class MessageReader(config: KafkaConsumerConfig) {
       }
     } yield personList
   }
-
-//  def processForAllPartitions(
-//      topic: String,
-//      partitions: List[Int],
-//      offset: Int,
-//      count: Int
-//  ): List[Person] = {
-//    partitions.flatMap(process(topic, _, offset, count))
-//  }
-//  // The ZIO version of the process function that returns a ZIO of List[Person]
-//  private def process(
-//      topic: String,
-//      partition: Int,
-//      offset: Int,
-//      count: Int
-//  ): List[Person] = {
-//    val consumer = createConsumer(config.bootstrapServers, config.groupId)
-//    val topicPartition = new TopicPartition(topic, partition)
-//    consumer.assign(List(topicPartition).asJava)
-//    consumer.seek(topicPartition, offset)
-//
-//    var personList: List[Person] = List()
-//    var messagesProcessed = 0
-//    try {
-//      while (messagesProcessed < count) {
-//        val records: ConsumerRecords[String, String] =
-//          consumer.poll(Duration.ofMillis(1000))
-//        if (records.isEmpty) {
-//          return personList
-//        }
-//        for (record <- records.asScala) {
-//          if (messagesProcessed < count) {
-//            decode[Person](record.value()) match {
-//              case Right(person) =>
-//                personList = personList :+ person
-//                messagesProcessed += 1
-//              case Left(error) =>
-//                println(
-//                  s"Error parsing message: ${record.value()}, error: $error"
-//                )
-//            }
-//          }
-//        }
-//      }
-//    } finally {
-//      consumer.close()
-//    }
-//    personList
-//  }
 }
